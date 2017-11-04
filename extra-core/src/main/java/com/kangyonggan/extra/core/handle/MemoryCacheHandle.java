d85fd8045805c296c030c3ae59c2cde46f5d998f
康永敢 <kangyonggan@gmail.com>
2017-11-04 22:07:55 +0800

    MemoryCacheHandle

--- extra-core/src/main/java/com/kangyonggan/extra/core/handle/MemoryCacheHandle.java
+++ extra-core/src/main/java/com/kangyonggan/extra/core/handle/MemoryCacheHandle.java
@@ -1,23 +1,46 @@
 package com.kangyonggan.extra.core.handle;
 
+import com.kangyonggan.extra.core.model.CacheItem;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author kangyonggan
  * @since 2017/11/4 0004
  */
 public class MemoryCacheHandle implements CacheHandle {
 
+    private volatile static Map<String, CacheItem> caches = new HashMap();
+
     @Override
     public Object set(String key, Object value, Long expire) {
+        caches.put(key, new CacheItem(value, expire));
         return value;
     }
 
     @Override
     public Object get(String key) {
-        return null;
+        CacheItem cacheItem = caches.get(key);
+        if (cacheItem == null) {
+            return null;
+        }
+
+        if (cacheItem.isExpire()) {
+            // remove expire cache
+            caches.remove(key);
+            return null;
+        }
+
+        // update cache item's  updateTime
+        cacheItem.setUpdateDate(new Date());
+
+        return cacheItem.getValue();
     }
 
     @Override
     public void delete(String key) {
-
+        caches.remove(key);
     }
 }
